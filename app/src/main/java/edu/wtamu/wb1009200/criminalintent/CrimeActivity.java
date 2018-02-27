c59e4869e65a184451ad88a4f18243fabbfb7333
William Bates <wbbates1@buffs.wtamu.edu>
2018-02-26 20:52:21 -0600

    Chapter 7 Challenge

--- app/src/main/java/edu/wtamu/wb1009200/criminalintent/CrimeActivity.java
+++ app/src/main/java/edu/wtamu/wb1009200/criminalintent/CrimeActivity.java
@@ -1,22 +1,25 @@
 package edu.wtamu.wb1009200.criminalintent;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+/*import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity; */
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
-        FragmentManager fm = getSupportFragmentManager();
+        FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = new Fragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
